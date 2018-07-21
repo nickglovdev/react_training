d7662591add55617a8196d45a56027bc1df7d1c0
Nicholas <jacksaintjack@gmail.com>
2018-07-21 14:17:40 -0500

    Went over react components. This gave better detail than the other class

---
+++ hellworld_react_comp/src/App.js
@@ -0,0 +1,42 @@
+/* XXX 1.
+import React from 'react';
+/* class App extends React.Component {
+  // Aways use render the return has JSX which is html like
+  // class is reserved name so need to use
+  //className to add a class name to a element
+  render() {
+  //gets compiled down to javascript
+    //return <h1>hello world</h1>
+  }
+}
+
+export default App */
+
+// XXX 2. React.createElement
+// import React from 'react';
+//
+// /* class App extends React.Component {
+//   // Aways use render the return has JSX which is html like
+//   // class is reserved name so need to use
+//   //className to add a class name to a element
+//   render() {
+//     XXX gets compiled down to javascript
+//     //return <h1>hello world</h1>
+//     XXX reacte.createElement(1,2,3) is javascript.
+//     //1. All custom react component or custom component in the first parameter
+//     //2. parameter
+//     //3. can be another element or string
+//
+//     // return React.createElement('h1', null, 'Hello Egghead' )
+//   }
+// }
+//
+//
+// export default App */
+
+// XXX 3. stateless function. Class can have state.
+import React from 'react';
+
+const App = () => <h1>Hello world</h1>
+
+export default App
